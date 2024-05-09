63a1263e0acf477d02ad24224f0e44aabb086bf7
Leandro <leandro.silva@sislogica.com.br>
2024-05-09 03:14:28 -0300

    first commit

---
+++ pages/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../pages/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {'azulPadrao': '#023D68', 'white': '#FFFFFF', 'preto': '#000000'}
+  },
+  plugins: [],
+}
+
