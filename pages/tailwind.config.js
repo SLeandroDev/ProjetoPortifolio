b5c298bb22ba631d38650755d0e190fa6a3cf693
Leandro <leandro.silva@sislogica.com.br>
2024-05-09 04:17:26 -0300

    ajuste

--- pages/tailwind.config.js
+++ pages/tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["../pages/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+      'shadowPadrao': '7px 4px 7px rgba(0, 0, 0, 0.31)'
+    }},
     colors: {'azulPadrao': '#023D68', 'white': '#FFFFFF', 'preto': '#000000'}
   },
   plugins: [],
