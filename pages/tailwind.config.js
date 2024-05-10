80e208ad6d93666d47bbe31658d8f9c1234d7d53
Leandro <leandro.silva@sislogica.com.br>
2024-05-10 01:16:07 -0300

    criação da seção 1

--- pages/tailwind.config.js
+++ pages/tailwind.config.js
@@ -2,10 +2,7 @@
 module.exports = {
   content: ["../pages/**/*.{html,js}"],
   theme: {
-    extend: {
-      dropShadow: {
-      'shadowPadrao': '7px 4px 7px rgba(0, 0, 0, 0.31)'
-    }},
+    extend: {},
     colors: {'azulPadrao': '#023D68', 'white': '#FFFFFF', 'preto': '#000000'}
   },
   plugins: [],
